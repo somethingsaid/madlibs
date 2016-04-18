7b9244b836c8b04be0525b12bded802cd9ec2dfc
somethingsaid <kwong.billy@gmail.com>
2016-04-17 22:05:45 -0400

    Added gender toggle for story

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 var app = angular.module('myApp', [])
 app.controller('madLibsController', function($scope) {
-	$scope.maleName = '<some name>';
-	$scope.dirtyTask = '<dirty task>';
-	$scope.obnoxCeleb = '<obnoxious celebrity>';
-	$scope.jobTitle = '<job title>';
-	$scope.celeb = '<celebrity>';
-	$scope.hugeNum = '<huge number>';
-	$scope.tediousTask = '<tedious task>';
-	$scope.uselessSkill = '<useless skill>';
-	$scope.adjective = '<adjective>';
+	$scope.reset = function() {
+		$scope.male = false;
+        $scope.charName = null;
+	    $scope.dirtyTask = null;
+	    $scope.obnoxCeleb = null;
+	    $scope.jobTitle = null;
+		$scope.celeb = null;
+		$scope.hugeNum = null;
+		$scope.tediousTask = null;
+		$scope.uselessSkill = null;
+		$scope.adjective = null;
+		$scope.showMadLibs = false;
+	}
+	$scope.reset();
 });
