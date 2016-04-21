9185b3f1f8fac12ed0db7d00aa52664be4993d20
somethingsaid <kwong.billy@gmail.com>
2016-04-21 14:02:03 -0400

    Added basic ng-animate

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', [])
+var app = angular.module('myApp', ['ngAnimate'])
 app.controller('madLibsController', function($scope) {
 	$scope.reset = function() {
 		$scope.isMale = false;
